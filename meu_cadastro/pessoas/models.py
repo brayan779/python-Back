05e3b4f9ca222e5500e9a65fb04f30766835dd15
brayan779 <brayan2.0yh@gmail.com>
2025-10-23 19:02:14 -0300

    Adicionei as classes no pessos/models.py

--- meu_cadastro/pessoas/models.py
+++ meu_cadastro/pessoas/models.py
@@ -8,3 +8,42 @@ class Pessoa(models.Model):
 
     def __str__(self):
         return self.nome
+
+# Arquivo: teste.py
+
+class Vendas:
+    def __init__(self, produto, valor, data, forma_pagamento):
+        self.produto = produto
+        self.valor = valor
+        self.data = data
+        self.forma_pagamento = forma_pagamento
+
+class Comanda:
+    def __init__(self, codigo, data, produtos, total):
+        self.codigo = codigo
+        self.data = data
+        self.produtos = produtos  # lista de produtos
+        self.total = total
+
+class ContasReceber:
+    def __init__(self, convenio, data, valor, identificacao, particulares, marmitas):
+        self.convenio = convenio
+        self.data = data
+        self.valor = valor
+        self.identificacao = identificacao
+        self.particulares = particulares
+        self.marmitas = marmitas
+
+class Suprimentos:
+    def __init__(self, embalagens, bebidas, comidas, temperos, produtos_limpeza):
+        self.embalagens = embalagens
+        self.bebidas = bebidas
+        self.comidas = comidas
+        self.temperos = temperos
+        self.produtos_limpeza = produtos_limpeza
+
+class Caixa:
+    def __init__(self, contas_receber, contas_pagar, forma_pagamento):
+        self.contas_receber = contas_receber  # pode ser lista de ContasReceber
+        self.contas_pagar = contas_pagar
+        self.forma_pagamento = forma_pagamento
